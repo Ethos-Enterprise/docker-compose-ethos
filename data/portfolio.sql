40ea0b4052e754b84dcb034f23b7596378ba0edd
lealivia <livia.leal@sptech.school>
2024-03-19 21:31:33 -0300

    Fix: Corrigindo tabela de portfolio

--- data/portfolio.sql
+++ data/portfolio.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Portfolio (
     id uuid PRIMARY KEY,
     url_imagem_perfil TEXT,
-    url_backgroud_perfil TEXT,
+    url_background_perfil TEXT,
     descricao_empresa VARCHAR(300),
     sobre_empresa VARCHAR(1000),
     link_website_empresa VARCHAR(300),
-    data_empresa_certificado TIMESTAMP,
+    data_empresa_certificada TIMESTAMP,
     fk_prestadora_servico UUID
     );
