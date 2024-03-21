9aedfd0ca627ec61e682d17b479b3845da320482
lealivia <livia.leal@sptech.school>
2024-03-20 22:27:27 -0300

    Fix: tabela de meta

--- data/meta.sql
+++ data/meta.sql
@@ -1,13 +1,13 @@
 CREATE TABLE META (
     id UUID PRIMARY KEY,
-    pilarEsg VARCHAR(100),
+    pilar_esg VARCHAR(100),
     descricao VARCHAR(100),
-    dataInicio DATE,
-    dataFim DATE,
+    data_inicio DATE,
+    data_fim DATE,
     fk_empresa UUID
 );
 
-INSERT INTO META (id, pilarEsg, descricao, dataInicio, dataFim, fk_empresa)
+INSERT INTO META (id, pilar_esg, descricao, data_inicio, data_fim, fk_empresa)
 VALUES
     ('f7a6f0c7-7f14-4e4d-bd69-563a6656a17e', 'Ambiental', 'Descrição da meta Ambiental', '2024-03-01', '2024-06-30', '6ba7b810-9dad-11d1-80b4-00c04fd430c8'),
     ('4e70fe29-0738-4a7d-b6ef-4accc0ad172e', 'Social', 'Descrição da meta Social', '2024-04-01', '2024-09-30', '6ba7b810-9dad-11d1-80b4-11c04fd430c7');
