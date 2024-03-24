c547fc429b0c8ab5ba2b0cc37c1052c62f693060
Viviana Marca <viviana.marca@sptech.school>
2024-03-23 23:51:05 -0300

    add insercoes

--- data/prestadora.sql
+++ data/prestadora.sql
@@ -3,3 +3,7 @@ create TABLE IF NOT EXISTS PRESTADORA_SERVICO (
   status_aprovacao varchar(30),
   fk_empresa UUID unique
 );
+
+insert into PRESTADORA_SERVICO  values (
+'22724577-0edf-49ef-a56d-2b35c648b224', 'APROVADO', '6ba7b813-9dad-11d1-80b4-00c04fd430c4'
+);
