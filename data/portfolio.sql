80f71d059bc9770744eaa95fe7c832b5326826a2
Jefferson <jefferson.silva@jazztech.com.br>
2024-05-06 20:15:22 -0300

    Adicionando variáveis de ambiente

--- data/portfolio.sql
+++ data/portfolio.sql
@@ -8,3 +8,5 @@ CREATE TABLE IF NOT EXISTS Portfolio (
     data_empresa_certificada TIMESTAMP,
     fk_prestadora_servico UUID
     );
+
+insert into Portfolio (id, url_imagem_perfil, url_background_perfil, descricao_empresa, sobre_empresa, link_website_empresa, data_empresa_certificada, fk_prestadora_servico) values ('f47b3b3b-1b3b-4b3b-8b3b-3b3b3b3b3b3b', 'https://www.google.com.br', 'https://www.google.com.br', 'Empresa de TI', 'Empresa de TI', 'https://www.google.com.br', '2021-10-10 00:00:00', '22724577-0edf-49ef-a56d-2b35c648b224');
